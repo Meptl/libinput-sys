4bf69ec8b18a965df4fc2bd0db26be3d3ab6436f
Christopher Chin <ctchin13@gmail.com>
2016-09-27 19:59:30 -0400

    init commit

---
+++ build.rs
@@ -0,0 +1,24 @@
+extern crate bindgen;
+
+use std::fs::File;
+use std::io::Write;
+
+const OUT_FILE: &'static str = concat!(env!("OUT_DIR"), "/libinput-sys.rs");
+
+pub fn main() {
+    let mut file = Box::new(File::create(OUT_FILE).expect("Unable to write outfile"));
+    write!(&mut file, "pub mod ffi {{").unwrap();
+
+    let mut builder =  bindgen::Builder::new("/usr/include/libinput.h");
+    builder.link("input", bindgen::LinkType::Dynamic);
+    builder.link("udev", bindgen::LinkType::Dynamic);
+    builder.builtins();
+
+    let bindings = match builder.generate() {
+        Ok(b) => b.to_string(),
+        Err(e) => panic!(e)
+    };
+
+    file.write_all(bindings.into_bytes().as_slice()).expect("failed to write bindings");
+    write!(&mut file, "}}").unwrap();
+}
---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+include!(concat!(env!("OUT_DIR"), "/libinput-sys.rs"));
+
+pub use ffi::*;
